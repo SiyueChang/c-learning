ca51f6a9e3ca6c18269893177cab94c0a7c88755
SiyueChang <40568213+SiyueChang@users.noreply.github.com>
2020-12-13 18:08:15 +0800

    2
    2

--- lesson2.cpp
+++ lesson2.cpp
@@ -31,14 +31,43 @@ void p3()
 	p3v2(); 
 }
 
-int p4(int age)
+void p4()
 {
-	cin<<
+	int age = 0;
+	cout<<"enter your age: ";
+	cin>>age;
+	cout<<endl;
+	cout<<"month is "<<age*12<<endl;
 }
 
+void p5()
+{
+	float dagrees;
+	cout<<"please enter a celsius value: ";
+	cin>>dagrees;
+	cout<<dagrees<<" degrees celsius is "<<dagrees*1.8+32.0<<" degrees fahrenheit."<<endl;
+}
+
+void p6()
+{
+	double units;
+	cout<<"enter the number of light years:";
+	cin>>units;
+	cout<<units<<"light years = "<<units*63240<<" astronomical units"<<endl;
+}
+
+void p7()
+{
+	int hours,minutes;
+	cout<<"enter the number of hours:";
+	cin>>hours;
+	cout<<"enter the number of minutes:";
+	cin>>minutes;
+	cout<<"time: "<<hours<<":"<<minutes<<endl;
+}
 int main()
 {
-	p3();
+	p7();
 	return 0;
 }
 
