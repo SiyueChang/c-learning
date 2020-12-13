7f762fa033e5809e4f444e268b0eec5ec830866c
SiyueChang <40568213+SiyueChang@users.noreply.github.com>
2020-12-13 17:49:26 +0800

    1

---
+++ lesson2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+void p1()
+{
+    cout<<"zhangsiyue"<<endl;
+	cout<<"china,jilin,animation college"<<endl;
+}
+
+void p2()
+{
+	float longm;
+	cout<<"输入long的长度:"<<endl; 
+    cin>>longm;
+    cout<<"long的长度为"<<longm*220<<"码"<<endl; 
+}
+
+void p3v1()
+{
+	cout<<"three blind mice"<<endl;
+}
+void p3v2()
+{
+	cout<<"see how they run"<<endl;
+}
+void p3()
+{
+	p3v1();
+	p3v1();
+	p3v2();
+	p3v2(); 
+}
+
+int p4(int age)
+{
+	cin<<
+}
+
+int main()
+{
+	p3();
+	return 0;
+}
+
