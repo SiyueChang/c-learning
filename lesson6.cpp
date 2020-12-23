3cce0a796d2c9f707bfcc6f89842020992fb6dce
SiyueChang <40568213+SiyueChang@users.noreply.github.com>
2020-12-23 20:13:47 +0800

    1

--- lesson6.cpp
+++ lesson6.cpp
@@ -1,3 +1,213 @@
+#include <iostream>
+#include <string>
+#include <array>
+#include <cstring>
+#include <cctype>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+void p1()
+{
+	char ch = 0;
+	while ((ch = cin.get()) != '@')
+	{
+		if (isdigit(ch))
+		{
+			continue;
+		}
+		else if (islower(ch))
+		{
+			cout << (char)toupper(ch);
+		}
+		else if (isupper(ch))
+		{
+			cout << (char)tolower(ch);
+		}
+	}
+
+	cout << "Done!" << endl;
+}
+
+
+void p2()
+
+{
+	double arr[12];
+	double sum = 0,avg = 0;
+	int enter = 0,lar = 0;
+	while (enter < 10 && (cin >> arr[enter]))
+	{
+		sum += arr[enter];
+		enter++;
+	}
+	avg = sum / enter;
+	for (int i = 0; i < enter;i++)
+	{
+		if (arr[i] > avg)
+		{
+			lar++;
+		}
+	}
+	cout << "The average value is " << avg << ", and there are " << lar << " double value large than average value!" << endl;
+}
+
+void p3()
+{
+	char ch = 0;
+	cout << "Please enter one of the following choices: " << endl;
+	cout << "c) carnivore" << "p) pianist" << endl;
+	cout << "t) tree" << "g) game" << endl;
+	bool exit = false;
+	while (cin >> ch && !exit)
+	{
+		switch (ch)
+		{
+		case 'c':
+			cout << "safsacsac." << endl;
+			exit = true;
+			break;
+
+		case 'p':
+			cout << "safcsadc." << endl;
+			exit = true;
+			break;
+
+		case 't':
+			cout << "A maple is a tree." << endl;
+			exit = true;
+			break;
+
+		case 'g':
+			cout << "sdfsc." << endl;
+			exit = true;
+			break;
+
+		default:
+			cout << "Please enter a c, p, t or g: ";
+			break;
+		}
+	}
+}
+void display_by_name(const struct bop *bopArray, unsigned int size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << bopArray[i].fullname << endl;
+	}
+}
+void display_by_title(const struct bop *bopArray, unsigned int size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << bopArray[i].title << endl;
+	}
+}
+void display_by_bopname(const struct bop *bopArray, unsigned int size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << bopArray[i].bopname << endl;
+	}
+}
+void display_by_preference(const struct bop *bopArray, unsigned int size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		if (bopArray[i].preference == 0)
+		{
+			cout << bopArray[i].fullname << endl;
+		}
+		else if (bopArray[i].preference == 1)
+		{
+			cout << bopArray[i].title << endl;
+		}
+		else if (bopArray[i].preference == 2)
+		{
+			cout << bopArray[i].bopname << endl;
+		}
+	}
+}
+const int strsize = 50;
+struct bop
+{
+	char fullname[strsize];     // real name
+	char title[strsize];        // job title
+	char bopname[strsize];      // secret BOP name
+	int preference;             // 0 = fullname, 1 = title, 2 = bopname
+};
+void p4()
+{
+	const struct bop bopArray[5] = {
+		{ "Wimp Macho", "YYY", "Y----", 0 },
+		{ "XXXXXXXX", "2XXXX", "3XXXXX", 1 },
+		{ "AAAAAAAA", "2AAAA", "3AAAAA", 2 },
+		{ "BBBBBBBB", "2BBBB", "3BBBBB", 0 },
+		{ "CCCCCCCC", "4CCCC", "3CCCCC", 1 }
+	};
+	char choice = 0;
+	cout << "Benevolent Order of Proframers Report" << endl;
+	cout << left << setw(30) << "a. display by name" << "b. display by title" << endl;
+	cout << left << setw(30) << "c. display by bopname" << "d. display by preference" << endl;
+	cout << left << setw(30) << "q. quit" << endl;
+	cout << "Enter your choice:";
+
+	while (cin >> choice)
+	{
+		if (choice == 'q')
+		{
+			break;
+		}
+		switch (choice)
+		{
+		case 'a':
+			display_by_name(bopArray, 5);
+			break;
+
+		case 'b':
+			display_by_title(bopArray, 5);
+			break;
+
+		case 'c':
+			display_by_bopname(bopArray, 5);
+			break;
+
+		case 'd':
+			display_by_preference(bopArray, 5);
+			break;
+
+		default:
+			break;
+		}
+		cout << "Next choice:";
+	}
+	cout << "Bye!" << endl;
+	return;
+}
+
+void p5()
+{
+	const double RATE1 = 0.1;
+	const double RATE2 = 0.15;
+	const double RATE3 = 0.2;
+	double income = 0.0;
+	double tax = 0.0;
+
+	cout << "Please enter your income:";
+	while ((cin >> income) && (income > 0))
+	{
+		if (income <= 5000)
+		{
+			tax = 0.0;
+		}
+		else if (5000 < income && income <= 15000)
+		{
+			tax = (income - 5000) * RATE1;
+		}
+		else if (15000 < income && income <= 35000)
+		{
+			tax = (15000 - 5000) * RATE1 + (income - 15000) * RATE2;
 		}
 		else
 		{
